72ac33daf93ab32ef18cda5958bf2f335fc5b159
Erik Neves <erikrrn04@gmail.com>
2022-02-26 18:50:14 -0300

    Models/Equipe

---
+++ team-manager2/Models/Equipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team_manager2.Models
+{
+    public class Equipe
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public List<int> FuncionariosId = new List<int>();
+
+        public Equipe(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public Equipe(string nome, int[] idFuncionarios)
+        {
+            this.Nome = nome;
+            foreach (int item in idFuncionarios) this.FuncionariosId.Add(item);
+        }
+    }
+}
