2ebede0506954b4f3896755afe3857ea9d3ab126
Erik Neves <erikrrn04@gmail.com>
2022-02-26 16:47:17 -0300

    Models/funcionário

---
+++ team-manager2/Models/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team_manager2.Models
+{
+    public class Funcionario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cargo { get; set; }
+        public string email { get; set; }
+        public string equipe_id { get; set; }
+
+        public Funcionario(int Id, string Nome, string Cargo, string Equipe_id, string Email = null)
+        {
+            this.id = Id;
+            this.nome = Nome;
+            this.cargo = cargo;
+            this.equipe_id = Equipe_id;
+            this.email = Email;
+        }
+    }
+}
