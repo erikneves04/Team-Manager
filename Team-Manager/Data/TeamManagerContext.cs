997615781e9e4a3bc8fbd95e742669718a03307e
Erik Neves <erikrrn04@gmail.com>
2022-03-05 13:38:00 -0300

    Inclusão de classe de contexto

---
+++ Team-Manager/Data/TeamManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Team_Manager.Models;
+
+namespace Team_Manager.Data
+{
+    public class TeamManagerContext : DbContext
+    {
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Equipe> Equipes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=TeamManager;Data Source=ERIK");
+        }
+
+    }
+}
