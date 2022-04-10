9ef64d6124e9b20af690dc16f3a558d2df00f6c7
Erik Neves <erikrrn04@gmail.com>
2022-04-09 21:12:24 -0300

    Update Models

---
+++ Team-Manager/Domain/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace Team_Manager.Domain.Models;
+
+public class Employee : BaseModel
+{
+    public Employee() { }
+
+    public Employee(string name, string function, Guid teamId, string email = null)
+    {
+        Name = name;
+        Function = function;
+        Email = email;
+        TeamId = teamId;
+    }
+
+    public string Name { get; set; }
+    public string Function { get; set; }
+    public string Email { get; set; }
+    public Guid TeamId { get; set; }
+    public Team Team { get; set; }
+}
---
+++ Team-Manager/Domain/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿namespace Team_Manager.Domain.Models;
+
+public class Team : BaseModel
+{
+    public string Name { get; set; }
+    public string Sector { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+
+    public Team(string name, string sector)
+    {
+        Name = name;
+        Sector = sector;
+    }
+}
