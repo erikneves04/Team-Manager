53f8926833a0c85a6c51516632237bef72c5976b
Erik Neves <erikrrn04@gmail.com>
2022-04-09 21:18:08 -0300

    Create Team ViewModels

---
+++ Team-Manager/Domain/ViewModels/TeamViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Team_Manager.Domain.Models;
+
+namespace Team_Manager.Domain.ViewModels;
+
+public abstract class TeamBaseViewModel
+{
+    public string Name;
+    public string Sector;
+    public string Description;
+}
+
+public class TeamViewModel : TeamBaseViewModel
+{
+    public TeamViewModel() { }
+    public TeamViewModel(Guid id, string name, string sector, string description, ICollection<Employee> employees = null)
+    {
+        Id = id;
+        Name = name;
+        Sector = sector;
+        Description = description;
+        Employees = employees;
+    }
+
+    public Guid Id { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+}
+
+public class TeamInsertUpdateViewModel : TeamBaseViewModel
+{
+    public TeamInsertUpdateViewModel() { }
+    public TeamInsertUpdateViewModel(string name, string sector, string description)
+    {
+        Name = name;
+        Sector = sector;
+        Description = description;
+    }
+}
