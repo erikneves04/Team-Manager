cec7b25895a6f37d895f3a028d7ae08725ae2184
Erik Neves <erikrrn04@gmail.com>
2022-04-10 10:50:07 -0300

    Create TeamController.cs

---
+++ Team-Manager/Controllers/TeamController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Team_Manager.Domain.Services;
+using Team_Manager.Domain.ViewModels;
+
+namespace Team_Manager.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TeamController : ControllerBase
+{
+    private readonly TeamServices _services;
+
+    public TeamController(TeamServices services)
+    {
+        _services = services;
+    }
+
+    [HttpGet]
+    public ActionResult<ICollection<TeamViewModel>> GetAll()
+    {
+        var content = _services.GetAll();
+        if (content == null)
+            return NotFound("Nenhum time foi encontrado.");
+
+        return new ActionResult<ICollection<TeamViewModel>>(content);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<TeamViewModel> Get(Guid id)
+    {
+        var content = _services.GetViewModel(id);
+        if (content == null)
+            return NotFound("O time não foi encontrado.");
+
+        return new ActionResult<TeamViewModel>(content);
+    }
+
+    [HttpPost]
+    public ActionResult<TeamViewModel> Post(TeamInsertUpdateViewModel model)
+    {
+        try
+        {
+            var content = _services.Insert(model);
+            return new ActionResult<TeamViewModel>(content);
+        }
+        catch (InvalidDataException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<TeamViewModel> Put(TeamInsertUpdateViewModel model, Guid id)
+    {
+        try
+        {
+            var content = _services.Update(model, id);
+            return new ActionResult<TeamViewModel>(content);
+        }
+        catch (InvalidDataException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<NoContentResult> Delete(Guid id)
+    {
+        try
+        {
+            _services.Delete(id);
+            return Ok();
+        }
+        catch (InvalidDataException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
