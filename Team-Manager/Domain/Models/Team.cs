6dcd5aad1b3514379773ff0707567f88696bd02d
Erik Neves <erikrrn04@gmail.com>
2022-04-09 21:14:28 -0300

    Create Team description

--- Team-Manager/Domain/Models/Team.cs
+++ Team-Manager/Domain/Models/Team.cs
@@ -2,13 +2,15 @@
 
 public class Team : BaseModel
 {
-    public string Name { get; set; }
-    public string Sector { get; set; }
-    public ICollection<Employee> Employees { get; set; }
-
-    public Team(string name, string sector)
+    public Team(string name, string sector, string description)
     {
         Name = name;
         Sector = sector;
+        Description = description;
     }
+
+    public string Name { get; set; }
+    public string Sector { get; set; }
+    public string Description { get; set; }
+    public ICollection<Employee> Employees { get; set; }
 }
